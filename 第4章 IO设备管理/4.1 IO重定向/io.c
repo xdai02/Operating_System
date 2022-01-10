6878cefaf8d5d5a4a1ac7cf15b48e27c190e9ad7
xdai02 <2513276112@qq.com>
2022-01-10 23:12:48 +0800

    format chapter 3 and 4

--- 第4章 IO设备管理/4.1 IO重定向/io.c
+++ 第4章 IO设备管理/4.1 IO重定向/io.c
@@ -2,8 +2,9 @@
 
 int main(int argc, char *argv[]) {
     int num;
+    printf("(stdin) enter an integer: ");
     fscanf(stdin, "%d", &num);
-    fprintf(stdout, "[stdout] num = %d\n", num);
-    fprintf(stderr, "[stderr] This is an error message.\n");
+    fprintf(stdout, "(stdout) num = %d\n", num);
+    fprintf(stderr, "(stderr) This is an error message.\n");
     return 0;
 }
